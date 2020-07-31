cbc1be4f8501144a23674e0177bbe01731bf6eba
Bruno <brunnofn19@gmail.com>
2020-07-31 19:01:16 -0300

    initial structure added

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import { AppError } from 'errors/AppError';
+
+const app = express();
+app.use(cors());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333, () =>
+  console.log('[SERVER]: running on http://locahost:3333'),
+);
